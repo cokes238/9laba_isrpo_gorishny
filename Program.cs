7dc28d959ad44463419686073f88fa4f916f94e3
cokes238 <cokes238@gmail.com>
2025-10-23 19:48:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _9laba_isrpo_gorishny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
